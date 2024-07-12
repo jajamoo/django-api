869daa0efaeeb81bad8a061fa7d60960bf0300fc
Moe Kahn <jajamoo@gmail.com>
2024-07-12 14:13:14 -0400

    progress

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,7 @@
+"""Command to wait for DB to start"""
+
+from django.core.management import BaseCommand
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        pass
---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,11 @@
+from unittest.mock import patch
+from psycog
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
